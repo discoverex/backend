67daa67f48a5a7e699e4d6dc9df03750caf0ef23
김광회 <postelian@gmail.com>
2026-02-27 21:39:32 +0900

    fix: UserInfo is not JSON serializable

--- src/domains/auth/dtos/auth_dtos.py
+++ src/domains/auth/dtos/auth_dtos.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class UserInfo(BaseModel):
-    uid: str = Field(..., description="파이어베이스 유저 고유 아이디"),
+    uid: str = Field(..., description="파이어베이스 유저 고유 아이디")
     email: str = Field(..., description="사용자 이메일")
