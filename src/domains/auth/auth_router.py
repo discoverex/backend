65037814497f78976dc05de9ea12665d120b1c17
elian118 <elian118@naver.com>
2026-02-27 17:08:19 +0900

    feat: 인증 반환타입 변경

--- src/domains/auth/auth_router.py
+++ src/domains/auth/auth_router.py
@@ -9,7 +9,7 @@ auth_router = APIRouter(prefix="/auth", tags=["인증"])
 @auth_router.get(
     "/users/me",
     response_model=WrappedResponse[UserInfo],
-    summary="주사위 판정 실행",
+    summary="로그인",
     description="2d6 주사위를 굴려 플레이어의 능력치를 더한 후, 설정된 난이도와 비교해 성공 여부를 판정합니다.",
 )
 async def get_my_profile(user_info: dict = Depends(verify_firebase_token)):
@@ -20,8 +20,4 @@ async def get_my_profile(user_info: dict = Depends(verify_firebase_token)):
     # 여기서 우리 DB(PostgreSQL 등)를 조회하여 추가 정보를 가져올 수 있습니다.
     # 예: user_data = db.query(User).filter(User.firebase_uid == uid).first()
 
-    return {
-        "message": "인증 성공!",
-        "firebase_uid": uid,
-        "email": email
-    }
+    return UserInfo(message="인증 성공!", firebase_uid=uid, email=email)
